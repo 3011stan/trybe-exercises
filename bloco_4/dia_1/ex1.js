2811f1aadacd2dd1ddfb5ea42831bd1b05897c02
Stanrley Barbosa <3011stan@gmail.com>
2021-02-24 19:35:20 -0300

    Resoluçao exercicio 1, 2 e 3 do dia 4.1

---
+++ bloco_4/dia_1/ex1.js
@@ -0,0 +1,22 @@
+let a = 10;
+let b = 5;
+
+//adição
+let soma = b + a;
+console.log('A soma dos valores é '+soma);
+
+//subtração
+let subtração = a - b;
+console.log('A subtração entre os valores é '+subtração);
+
+//multiplicação
+let multi = b*a;
+console.log('A multiplicação entre os valores é '+multi);
+
+//divisão
+let div = a/b;
+console.log('A divisão entre os valores é '+div);
+
+//modulo
+let mod = a%b;
+console.log("O módulo entre os dois numeros é "+mod);
---
+++ bloco_4/dia_1/ex2.js
@@ -0,0 +1,8 @@
+let a = 1;
+let b = 5;
+
+if(a > b) {
+  console.log('O numero maior é o '+a);
+}else if(b > a) {
+  console.log('O numero maior é o '+b);
+}
