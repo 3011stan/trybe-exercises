7738717e4799f54c0a0efd5a4ddb43393b47d45d
Stanrley Barbosa <3011stan@gmail.com>
2021-02-24 20:15:10 -0300

    Resoluçao exercicio 5

---
+++ bloco_4/dia_1/ex5.js
@@ -0,0 +1,10 @@
+let a = 10;
+let b = 70;
+let c = 100;
+let soma = (a + b) + c;
+
+if(soma == 180){
+  console.log("As somas internas correspondem a um triangulo.");
+}else {
+  console.log('As somas não correspondem a um triangulo.');
+}
