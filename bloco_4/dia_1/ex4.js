6cd3da060ed42473566d5e50f0140181f09a779b
Stanrley Barbosa <3011stan@gmail.com>
2021-02-24 19:37:53 -0300

    Resoluçao exercicio 4 do dia 4.1

---
+++ bloco_4/dia_1/ex4.js
@@ -0,0 +1,8 @@
+let positiveOrNegative = 10;
+if(positiveOrNegative > 0){
+  console.log('Numeo positivo');
+}else if(positiveOrNegative < 0){
+  console.log('Numero negativo');
+}else if(positiveOrNegative == 0){
+  console.log('Numero igual a zero.');
+}
