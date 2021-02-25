4bb6591d5e9a747ed39623e597106c81668cfe26
Stanrley Barbosa <3011stan@gmail.com>
2021-02-25 15:04:06 -0300

    Exercicio 1 do dia 4.2

---
+++ bloco_4/dia_2/script.js
@@ -0,0 +1,7 @@
+let tarefas = ['Estudar Uvv', 'Estudar Trybe', 'Correr'];
+console.log(tarefas);
+tarefas.unshift('Jogar bola'); //inserindo valor 'Jogar bola' no inicio do array
+console.log(tarefas);
+tarefas.pop();  //removendo ultima posição do array.
+console.log(tarefas);
+console.log(tarefas.indexOf('Estudar Uvv')); //obtendo indice em que se encontra o dado 'Estudar Uvv'
