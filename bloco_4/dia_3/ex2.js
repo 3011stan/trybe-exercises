aa038b39cbf12129eaf21ae6490da1e26343acd2
Stanrley Barbosa <3011stan@gmail.com>
2021-02-26 18:41:59 -0300

    Resoluçao do exercicio 2 do dia 4.3

--- bloco_4/dia_3/ex2.js
+++ bloco_4/dia_3/ex2.js
@@ -1,3 +1,7 @@
+/** 2 - Para o segundo exercicio, faça o mesmo que antes, mas que imprima um triangulo com 5 asteriscos de base */
 const n = 5;
 let line = '';
-for()
+for(let j = 0; j < 5; j += 1){
+  line = line+'*';
+  console.log(line);
+}
