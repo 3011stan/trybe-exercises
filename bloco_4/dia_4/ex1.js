bb065743e72514b7c9771579a8762e060488efbf
Stanrley Barbosa <3011stan@gmail.com>
2021-03-01 18:50:11 -0300

    Exercicios 4 e 5 do dia 4.4

--- bloco_4/dia_4/ex1.js
+++ bloco_4/dia_4/ex1.js
@@ -1,3 +1,4 @@
+//Exercícios de objetos e for/in
 let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
@@ -5,13 +6,40 @@ let info = {
 };
 
 //Exercício 1
-console.log('Bem-vinda ' + info.personagem);
+console.log('Bem-vinda ' + info.personagem + '\n');
 
 //Exercício 2
 info['recorrente'] = 'Sim';
 console.log(info);
+console.log('\n');
 
 //Exercício 3
 for(let key in info){
   console.log(key);
 }
+console.log('\n');
+
+//Exercicio 4
+for(let key in info){
+  console.log(info[key]);
+}
+console.log('\n');
+
+//Exercicio 5
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dell`s Four Color Comics#178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
+};
+function showInfos(infoA, infoB){
+  console.log(info.personagem + ' e ' + info2.personagem);
+  console.log(info.origem + ' e ' + info2.origem);
+  console.log(info.nota + ' e ' + info2.nota);
+  if(info.recorrente === 'Sim' && info2.recorrente === 'Sim'){
+    console.log('Ambos recorrentes.');
+  }else{
+    console.log('Algum não é recorrente');
+  }
+}
+showInfos(info, info2);
