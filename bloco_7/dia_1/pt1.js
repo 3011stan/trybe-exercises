bf29531ef7d3a7cfbebf229a121dd058aa725de6
Stanrley Barbosa <3011stan@gmail.com>
2021-03-23 22:01:20 -0300

    Exercicio 2 da parte 1 do dia 7.1

--- bloco_7/dia_1/pt1.js
+++ bloco_7/dia_1/pt1.js
@@ -8,5 +8,9 @@ const testingScope = (escopo) => {
     console.log(elseScope);
   }
 }
-
 testingScope(true);
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+console.log(`Os números ${oddsAndEvens[5]}, ${oddsAndEvens[1]},${oddsAndEvens[2]},${oddsAndEvens[4]},${oddsAndEvens[3]},${oddsAndEvens[0]} se encontram ordenados de forma crescente!`);
+// Seu código aqui.
